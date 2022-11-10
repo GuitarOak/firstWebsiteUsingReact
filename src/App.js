db311a73ce9f05704611745e382676f220f2b2a7
Emil Karlsson <emil.h.karlsson@gmail.com>
2022-11-11 00:47:53 +0100

    Started with navbar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
-
+import React from 'react'
+import Navbar from './components/Navbar';
+ import {BrowserRouter as Router} from 'react-router-dom'
+import { Routes ,Route } from 'react-router-dom';
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
+    <>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' exact />
+      </Routes>
+    </Router>
+    </>
   );
 }
 
